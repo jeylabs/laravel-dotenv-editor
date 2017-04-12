9a3636f6f8f5aa6f16666876ee04f8a2ac66ac79
Ratheep <ratheep.ceymplon@jeylabs.com>
2017-04-12 16:13:37 +0530

    upload env token missing fixed

--- src/views/overview.blade.php
+++ src/views/overview.blade.php
@@ -304,9 +304,11 @@ All needed files are included within this file, so nothing could break if you ex
                                 </span>
                                 </p>
                                 <form method="post" action="{{ url(config("dotenveditor.route") . "/upload") }}" enctype="multipart/form-data">
+					<input type="hidden" name="_token" value="{!! csrf_token() !!}">
                                     <div class="form-group">
                                         <label for="backup">{!! trans('dotenv-editor::views.upload_label') !!}</label>
                                         <input type="file" name="backup">
+					
                                     </div>
                                     <button type="submit" class="btn btn-primary" title="Ein Backup von deinem Computer hochladen">
                                         {!! trans('dotenv-editor::views.upload_button') !!}
